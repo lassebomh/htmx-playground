6acd0435c82cb57ace3705ff2f9803006ebab4f2
Lasse <lasse@bomh.net>
2022-12-03 13:06:28 +0100

    Update base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: "./docs"
-  }
+    outDir: "./docs",
+
+  },
+  base: "/htmx-sandbox/"
 })
