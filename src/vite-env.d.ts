23375860a61116b7508db6f65cb365054e9e5bd7
Lasse <lasse@bomh.net>
2022-12-21 21:04:30 +0100

    Add thirdparty module declaration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,4 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
 
-// declare module "nunjucks" {
-//     let nunjucks: any = {}
-//     export default nunjucks;
-// }
-  
+let nunjucks: any = {};
