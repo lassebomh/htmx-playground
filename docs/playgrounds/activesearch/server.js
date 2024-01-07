307c597ea73e8ac131deef566944b7f00fa84ed0
Lasse H. Bomholt <lasse@bomh.net>
2024-01-07 20:46:44 +0100

    Build

---
+++ docs/playgrounds/activesearch/server.js
@@ -0,0 +1,39 @@
+import { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
+import Fuse from 'https://cdn.jsdelivr.net/npm/fuse.js@7.0.0/dist/fuse.mjs'
+
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+
+faker.seed(42)
+
+let contacts = []
+
+for (let i = 0; i < 300; i++) {
+    contacts.push({
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+    })
+}
+
+const fuse = new Fuse(contacts, {
+    keys: ['firstName', 'lastName'],
+    includeScore: true
+})
+
+on.get("/", (request) => {
+    return render(request, 'index.html', {
+        results: []
+    })
+})
+
+on.post("/search", async (request) => {
+    let formData = await request.formData()
+    let query = formData.get('search')
+
+    await sleep(1000)
+
+    let results = fuse.search(query)
+
+    return render(request, 'results.html', {
+        results: results.slice(0, 8)
+    })
+})
---
+++ docs/playgrounds/infinitescroll/server.js
@@ -0,0 +1,39 @@
+
+import { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
+
+function getContacts(page) {
+    faker.seed(page)
+
+    let contacts = []
+
+    for (let i = 0; i < 5; i++) {
+        contacts.push({
+            name: faker.person.fullName(),
+            phoneNr: faker.phone.number(),
+        })
+    }
+
+    return contacts
+}
+
+on.get("/", (request) => {
+
+    let context = {
+        contacts: getContacts(1),
+        nextPage: 2
+    }
+
+    return render(request, 'index.html', context)
+})
+
+on.get("/contacts", (request, page) => {
+
+    page = parseInt(page)
+
+    let context = {
+        contacts: getContacts(page),
+        nextPage: page+1,
+    }
+
+    return render(request, 'loadmore.html', context)
+})
